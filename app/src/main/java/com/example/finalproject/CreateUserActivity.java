03fcc83eaa2e0c16689f8349dc048edf1e7e34dc
TayveonH <Tayveonholmes@yahoo.com>
2023-03-27 19:51:23 -0400

    Created Re enter pass, also checks if passwords match. also did the required XML updates for it.

--- app/src/main/java/com/example/finalproject/CreateUserActivity.java
+++ app/src/main/java/com/example/finalproject/CreateUserActivity.java
@@ -13,6 +13,7 @@ public class CreateUserActivity extends AppCompatActivity {
 
     private EditText usernameEditText;
     private EditText passwordEditText;
+    private EditText reEnterPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +23,19 @@ public class CreateUserActivity extends AppCompatActivity {
         // Get references to the EditText fields
         usernameEditText = findViewById(R.id.user_Name);
         passwordEditText = findViewById(R.id.user_Password);
-
-        //TODO Maybe we have TWO passwordEditText, so that the user has to enter the same password twice, like most create account things on websites, app etc.
-        // E.g. basically just a couple of extra lines, where one of them will be (passwordEditText1.getText().toString() == passwordEditText2.getText().toString())
-
-        //TODO Maybe we'll have an exit button (say they don't want to actually create an account) and this'll redirect you back to the MainActivity login page
-        // Probably don't need this, as I think Android user's nowadays are used to swiping back (or pressing the back button) if they want to exit or go back from a specific part of the app
-
-        //This is if they failed to create a new user (aka the User with that Username already exists)
-        Intent intent_fromSecond = getIntent();
-        boolean existingUser = intent_fromSecond.getBooleanExtra("existingUser", false);
-        if (existingUser == true){
-            Toast.makeText(this, "User already Exists! Please try again.", Toast.LENGTH_SHORT).show();
-        }
+        reEnterPasswordEditText = findViewById(R.id.user_Password2);
 
         // Set up the button to create the user account
-        Button createAccountButton = findViewById(R.id.buttonCreate);
+        Button createAccountButton = findViewById(R.id.create_button);
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
+                String reEnterPassword = reEnterPasswordEditText.getText().toString();
 
                 // Check if the username and password are valid
-                if (isValidUsername(username) && isValidPassword(password)) {
+                if (isValidUsername(username) && isValidPassword(password) && isMatchedPassword(password, reEnterPassword)) {
 
                     // After creating the user account, we go directly to the SecondActivity
                     Intent intent = new Intent(CreateUserActivity.this, SecondActivityCourse.class);
@@ -56,8 +46,6 @@ public class CreateUserActivity extends AppCompatActivity {
                     finish();
                 } else {
                     // Show an error message
-                    //TODO ~Thomson, I see that you specified the length to be at least 8. Maybe we do a specialized message so that the User knows it has to be of length 8
-                    // Or display it somewhere on the UI, could be simply in the "hint" of the password UI
                     Toast.makeText(CreateUserActivity.this, "Invalid username or password. Please try again.", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -65,13 +53,19 @@ public class CreateUserActivity extends AppCompatActivity {
     }
 
     private boolean isValidUsername(String username) {
-        // TODO: Implement your own validation logic for the username
+        // Implement your own validation logic for the username
         return !username.isEmpty();
     }
 
     private boolean isValidPassword(String password) {
-        // TODO: Implement your own validation logic for the password
+        // Implement your own validation logic for the password
         return password.length() >= 8;
     }
+
+    private boolean isMatchedPassword(String password, String reEnterPassword) {
+        // Implement your own validation logic for the re-entered password
+        return password.equals(reEnterPassword);
+    }
 }
+
 
